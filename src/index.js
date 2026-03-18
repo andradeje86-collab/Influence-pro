ce6c616d271df18a201323bb06d889d119a444f5
andradeje86-collab <andradeje86@gmail.com>
2026-03-17 21:19:57 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import InfluencePRO from './InfluencePRO';
-
+import App from '../InfluencePRO';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
